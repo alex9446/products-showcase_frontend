c0fad334d98c74c25f7f4a335e759c1a8a93bb4a
Alessio Margelli <alex9446@gmail.com>
2021-06-14 00:50:15 +0200

    🚪 Hided logout icon if the user is not logged

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,12 @@
 import { mdiAccount, mdiLogoutVariant } from '@mdi/js';
 import { PageProps } from 'gatsby';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { FAKE_BOXES } from '../utils/fake-data';
 import { getProductIdFromSearch, sortAscByPosition } from '../utils/mixed';
 
 import BoxList from '../components/box-list/box-list';
+import { checkLogin } from '../utils/auth';
 import Head from '../components/head';
 import LinkIcon from '../components/icon/link-icon';
 import PageHeading from '../components/page-heading';
@@ -13,6 +14,13 @@ import ProductModal from '../components/product-modal/product-modal';
 
 export default function Index(props: PageProps) {
     const [products, setProducts] = useState(() => sortAscByPosition(FAKE_BOXES.slice(0,6)));
+    const [showLogout, setShowLogout] = useState(true);
+
+    // Check if the user is not logged
+    useEffect(() => {
+        checkLogin()
+        .catch(() => setShowLogout(false));
+    }, []);
 
     const searchedProductId = getProductIdFromSearch(props.location.search);
     const selectedProduct = products.find(product => product.id === searchedProductId);
@@ -27,11 +35,13 @@ export default function Index(props: PageProps) {
                     path={mdiAccount}
                     class="profile-icon corner-icon"
                 />
-                <LinkIcon
-                    to="/logout"
-                    path={mdiLogoutVariant}
-                    class="logout-icon corner-icon"
-                />
+                {showLogout && 
+                    <LinkIcon
+                        to="/logout"
+                        path={mdiLogoutVariant}
+                        class="logout-icon corner-icon"
+                    />
+                }
                 <PageHeading>Products Showcase</PageHeading>
                 <BoxList>{products}</BoxList>
             </div>
